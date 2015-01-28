03dab253dfe5271686ea8f730a7478e70de3ec8f
John G. Bandy <jack.bandy@cslab13.wheaton.edu>
2015-01-28 15:17:25 -0600

    Fleshed out skeletons for all Test.py files

---
+++ PyCamellia/IPTest.py
@@ -0,0 +1,15 @@
+import IP
+import unittest
+
+class IPTest(unittest.TestCase):
+  """Test something"""
+  #def testadd(self):
+    #self.assertAlmostEqual(8,Adder.addNumbers(5,3),delta=1e-12)
+    #self.assertEqual(8,Adder.addNumbers(5,3))
+  
+
+# Run the tests:
+if (__name__ == '__main__'):
+  unittest.main()
+
+
