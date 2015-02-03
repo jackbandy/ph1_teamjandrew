676350c33d01c6bfd5070acafe9c4a502e74b1ef
Jack Bandy <jackbandy@icloud.com>
2015-02-02 20:38:58 -0600

    Added 'Tests' directory with starting point for Python unit tests, unit test boilerplate

---
+++ Tests/BFTest.py
@@ -0,0 +1,14 @@
+import BF
+import unittest
+
+class BFTest(unittest.TestCase):
+  """Test something"""
+  #def testadd(self):
+    #self.assertAlmostEqual(8,Adder.addNumbers(5,3),delta=1e-12)
+    #self.assertEqual(8,Adder.addNumbers(5,3))
+  
+
+# Run the tests:
+if (__name__ == '__main__'):
+  unittest.main()
+
---
+++ Tests/MeshFactoryTest.py
@@ -0,0 +1,15 @@
+import MeshFactory
+import unittest
+
+class MeshFactoryTest(unittest.TestCase):
+  """Test something"""
+  #def testadd(self):
+    #self.assertAlmostEqual(8,Adder.addNumbers(5,3),delta=1e-12)
+    #self.assertEqual(8,Adder.addNumbers(5,3))
+  
+
+# Run the tests:
+if (__name__ == '__main__'):
+  unittest.main()
+
+
