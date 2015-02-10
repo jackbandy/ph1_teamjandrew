0df21fdd189e52f6439a0387fa1a02323699e52c
Jack Bandy <jackbandy@icloud.com>
2015-02-10 16:39:04 -0600

    Setup for IPTest.py is in place, awaiting test cases

--- PyCamellia/IPTest.py
+++ PyCamellia/IPTest.py
@@ -1,5 +1,36 @@
 import IP
+import LinearTerm
+import VarFactory
 import unittest
+import Solution
+import MeshFactory
+import Mesh
+import BF
+import Function
+from SpatialFilter import *
+
+
+varFac = VarFactory.VarFactory()
+fieldVar = varFac.fieldVar("fieldVar")
+testVar = varFac.testVar("testVar",3)
+testLT = LinearTerm.LinearTerm()
+testFlux = varFac.fluxVar("testFlux")
+
+allSpace = SpatialFilter.allSpace()
+allSpace.matchesPoint([1.0,1.0])
+
+spaceDim = 2
+useConformingTraces  = True
+poissonForm = PoissonFormulation.PoissonFormulation(spaceDim, useConformingTraces)
+stokesForm = StokesVGPFormulation.StokesVGPFormulation(spaceDim, useConformingTraces)
+
+testIP = IP.IP()
+testIP.addTerm(testVar)
+stokesForm.u(1) # VarPtr for x component of velocity
+p = stokesForm.p()   # VarPtr for pressure
+
+phi = poissonForm.phi() # VarPtr for main, scalar-valued variable in Poisson problem
+psi = poissonForm.psi() # VarPtr for gradient of psi, vector-valued
 
 class IPTest(unittest.TestCase):
   """Test something"""
