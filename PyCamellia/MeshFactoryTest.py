4ae590bbcf2ea7be71a35b46d16e43ce4a9a73d3
ajsudds <andrew.sutton@my.wheaton.edu>
2015-02-11 16:33:55 -0600

    changed MeshFactory.i MeshFactoryTest.py, got a set of tests for each of the methods

--- PyCamellia/MeshFactoryTest.py
+++ PyCamellia/MeshFactoryTest.py
@@ -11,31 +11,34 @@ testVar = varFac.testVar("t", Var.HGRAD)
 bf = BF.BF_bf(varFac)
 vecd = MeshFactory.vectord(2,1.5)
 veci = MeshFactory.vectori(2,1)
-mesh = MeshFactory.MeshFactory_rectilinearMesh(bf,vecd,veci,2) 
 
-print("Before refinement, mesh has %i active elements" % mesh.numActiveElements())
-
-mesh.hRefine([0])
-
-print("After refinement, mesh has %i active elements" % mesh.numActiveElements())
-numGlobalDofsBefore = mesh.numGlobalDofs();
-numElementsBefore = mesh.numElements();
-
-mesh.saveToHDF5("Mesh.HDF5")
-meshLoad = MeshFactory.MeshFactory_loadFromHDF5(bf,"Mesh.HDF5")
-
-
-numGlobalDofsAfter = meshLoad.numGlobalDofs();
-numElementsAfter = meshLoad.numElements();
 
 class MeshFactoryTest(unittest.TestCase):
   """Test something"""
-  def testadd(self):
-    #Tests to see if loadFromHDF5 works (assuming the saveToHDF5 works)
+  def testrectilinearMesh(self):
+    #Tests to see if we can even make a mesh
+    mesh = MeshFactory.MeshFactory_rectilinearMesh(bf,vecd,veci,2)
+    print ("Number of Elements in Mesh: %i" % mesh.numElements())
+    print ("Number of GlobalDofs in Mesh : %i" % mesh.numGlobalDofs())
+  def testloadFromHDF5(self):
+    #Tests to see if loadFromHDF5 works (assuming the saveToHDF5 works) 
+    mesh = MeshFactory.MeshFactory_rectilinearMesh(bf,vecd,veci,2)
+    numGlobalDofsBefore = mesh.numGlobalDofs();
+    numElementsBefore = mesh.numElements();
+
+    mesh.saveToHDF5("Mesh.HDF5")
+    meshLoad = MeshFactory.MeshFactory_loadFromHDF5(bf,"Mesh.HDF5")
+
+
+    numGlobalDofsAfter = meshLoad.numGlobalDofs();
+    numElementsAfter = meshLoad.numElements();
+
     self.assertEqual(numGlobalDofsBefore,numGlobalDofsAfter)
     self.assertEqual(numElementsBefore,numElementsAfter)
-  
 
+  def testReadTriangle(self):
+    #Tests to see if we can read a triangle mesh
+    triangleMesh = MeshFactory.MeshFactory_readTriangle("box.2.ele",bf,2,2)
 # Run the tests:
 if (__name__ == '__main__'):
   unittest.main()
